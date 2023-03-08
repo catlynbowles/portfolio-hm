679557399d7986b72481a901d46012f5f0f7db0a
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-03-08 11:00:00 -0700

    add gif and tailwind animation install

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,14 +1,21 @@
 import { Intro } from "./Intro";
 import { Navbar } from "./Navbar";
+import Image from "next/image";
+import catwalk from "../../public/assets/catwalk.gif";
 
 export const LandingPage = () => {
   return (
-    <section className="w-full h-full flex items-center justify-evenly">
-      <div className="">
-        <Intro />
+    <section>
+      <div>
+        <Image src={catwalk} alt="my gif" height={300} width={300} />
       </div>
-      <div className="">
-        <Navbar />
+      <div className="w-full h-full flex items-center justify-evenly">
+        <div className="">
+          <Intro />
+        </div>
+        <div className="">
+          <Navbar />
+        </div>
       </div>
     </section>
   );
